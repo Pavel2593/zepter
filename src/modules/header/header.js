1d57c98fb72b54df729b7d17a8eea40617eb0ab8
pavel2593 <pavelborisov2593@gmail.com>
2021-08-02 16:15:30 +0300

    Сверстаны все блоки, кроме блока отзывы

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -1,1 +1,23 @@
 console.log('header');
+$(document).ready(function () {
+    var burgerIcon = $('.header .header__burger-icon');
+    var closeIcon = $('.header .nav__close-icon');
+    var navContainer = $('.header .nav');
+    var navLink = $('.header .nav__link');
+    burgerIcon.on('click', function () {
+        navContainer.addClass('nav-show');
+    });
+    closeIcon.on('click', function () {
+        navContainer.removeClass('nav-show');
+    });
+
+    navLink.on('click', function () {
+        navContainer.removeClass('nav-show');
+    });
+
+    $(document).mouseup(function (event) {
+        if (!navContainer.is(event.target) && navContainer.has(event.target).length === 0) {
+            navContainer.removeClass('nav-show');
+        }
+    });
+});
